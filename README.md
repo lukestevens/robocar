3ec752eaea885b0f31ca2433ba16fd0dec9288cf
Luke Stevens <lukestevens@gmail.com>
2015-02-01 02:11:49 +0000

    Working. Better cleanup.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # robocar
 raspberry pi, node js car demo code
+
+todo:
+map some defaults for turning (250ms or so)
+better feedback
+case-insensitive commands
+list-command functions
+
--- car.js
+++ car.js
@@ -7,50 +7,68 @@ AllPins['LeftBk'] = 15;
 AllPins['RightFwd'] = 12;
 AllPins['RightBk'] = 11;
 
+function openAll(){
+  for (var p in AllPins){
+     gpio.open(AllPins[p], "output", function(err) {
+    });
+  }
+}
+
+function exit(){
+  if(cleanup()){
+    process.exit();
+  }
+}
+
+function reset(){  
+  if(cleanup()){
+    openAll();
+  }
+}
 function cleanup(){
   for (var p in AllPins){
    gpio.close(AllPins[p]);
    console.log(p + " is closed");
   }
+  return true;
 }
 
 // turn on a single motor for an amount of time
 function drive(pinName, howLong){
-  setPin(pinName, 1);
-  setTimeout(function(){
-    setPin(pinName, 0);
-  }, howLong);
+  if(setPin(pinName, 1)){
+    setTimeout(function(){
+      if(setPin(pinName, 0)){
+        console.log ('turned off');
+      }
+    }, howLong, pinName);
+  }
 }
 
 // self cleaning. open, set, close
 function setPin(pinName, pinValue){
-  gpio.open(AllPins[pinName], "output", function(err){
-    gpio.write(AllPins[pinName], pinValue, function(){
-      gpio.close(AllPins[pinName]);
-    });
-  });
+  gpio.write(AllPins[pinName], pinValue);
+  return true;
 }
 
 // direction helpers
 function goForward(howLong){
-  console.log('going forward for ' + howLong + ' seconds');
   drive('LeftFwd', howLong);
   drive('RightFwd', howLong);
 }
 
 function goBackward(howLong){
   drive('LeftBk', howLong);
-  drive('RightBk', howlong);
+  drive('RightBk', howLong);
 }
 
-function turnRight(howLong){
+function turnLeft(howLong){
   drive('RightBk', howLong);
   drive('LeftFwd', howLong);
 }
 
-function turnLeft(howLong){
+function turnRight(howLong){
   drive('LeftBk', howLong);
-  drive('RightBk', howLong);
+  drive('RightFwd', howLong);
 }
 
 // readline interface
@@ -64,10 +82,11 @@ function prompt(){
      var args = answer.split(" ");
      console.log(args);
      eval(args[0] + '(' + args[1] + ')');
-       
      rl.close;
      prompt();
   });
 }
 
+reset();
 prompt();
+
